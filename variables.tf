3e94b6eff0888605b2aad06492ca6b9e63770a99
koda <koda@MacBook-Air-de-koda.local>
2026-08-29 16:28:45 -0300

    feat: validate allowed AWS regions

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 variable "aws_region" {
   description = "Região AWS usada pelo projeto"
   type        = string
+
+  validation {
+    condition     = contains(["us-east-1"], var.aws_region)
+    error_message = "Environment deve ser dev, staging ou prod"
+  }
 }
